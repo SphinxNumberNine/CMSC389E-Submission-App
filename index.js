bf0fffa49b45c9fda4758396c7223e36d5bd7839
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2020-03-17 19:43:49 -0400

    little bit of cleanup

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
-app.use(cookieParser())
-app.use(fileUpload())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(fileUpload());
 
-require("./routes/test")(app)
+require("./routes/upload")(app);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
