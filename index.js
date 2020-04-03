4d3da2fa19cfd79e1452d265334858ae5735f5c1
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2020-04-02 21:35:04 -0400

    heroku fixes

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
-const errorHandler = require('errorhandler')
+const errorHandler = require("errorhandler");
 
 const app = express();
 
-app.use(errorHandler({ log: (err, str, req, res) => { console.log(str) }}))
+app.use(
+  errorHandler({
+    log: (err, str, req, res) => {
+      console.log(str);
+    }
+  })
+);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
@@ -16,6 +22,14 @@ app.use(cookieParser());
 app.use(fileUpload());
 
 require("./routes/upload")(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
